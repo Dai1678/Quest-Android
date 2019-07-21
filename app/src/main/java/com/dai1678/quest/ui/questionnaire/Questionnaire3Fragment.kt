adae36ed05f663271fca18855860aa99c7231579
Dai1678 <dai7774@gmail.com>
2019-07-21 21:38:49 +0900

    アンケート3問目のレイアウト作成中

--- app/src/main/java/com/dai1678/quest/ui/questionnaire/Questionnaire3Fragment.kt
+++ app/src/main/java/com/dai1678/quest/ui/questionnaire/Questionnaire3Fragment.kt
@@ -1,5 +1,6 @@
 package com.dai1678.quest.ui.questionnaire
 
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,22 +8,20 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
 import com.dai1678.quest.R
-import com.dai1678.quest.databinding.FragmentQuestionnaire2Binding
+import com.dai1678.quest.databinding.FragmentQuestionnaire3Binding
 
-class Questionnaire2Fragment : Fragment() {
+class Questionnaire3Fragment : Fragment() {
 
     private val viewModel: QuestionnaireViewModel by activityViewModels()
-    private lateinit var binding: FragmentQuestionnaire2Binding
+    private lateinit var binding: FragmentQuestionnaire3Binding
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_questionnaire2, container, false
+            inflater, R.layout.fragment_questionnaire3, container, false
         )
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
@@ -32,16 +31,7 @@ class Questionnaire2Fragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val navController = findNavController()
 
-        binding.questionnaire2BackButton.setOnClickListener {
-            navController.popBackStack()
-        }
-
-        binding.questionnaire2NextButton.setOnClickListener {
-            navController.navigate(
-                R.id.action_questionnaire2Fragment_to_questionnaire3Fragment
-            )
-        }
     }
+
 }
