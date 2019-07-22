f00311dd1b0d336b0fc9bd9c474310f8affe8c6d
Dai1678 <dai7774@gmail.com>
2019-07-22 22:41:03 +0900

    アンケート8問目のレイアウト、遷移処理完成

--- app/src/main/java/com/dai1678/quest/ui/questionnaire/Questionnaire8Fragment.kt
+++ app/src/main/java/com/dai1678/quest/ui/questionnaire/Questionnaire8Fragment.kt
@@ -2,27 +2,28 @@ package com.dai1678.quest.ui.questionnaire
 
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.dai1678.quest.R
-import com.dai1678.quest.databinding.FragmentQuestionnaire7Binding
 
-class Questionnaire7Fragment : Fragment() {
+import com.dai1678.quest.R
+import com.dai1678.quest.databinding.FragmentQuestionnaire8Binding
+
+class Questionnaire8Fragment : Fragment() {
 
     private val viewModel: QuestionnaireViewModel by activityViewModels()
-    private lateinit var binding: FragmentQuestionnaire7Binding
+    private lateinit var binding: FragmentQuestionnaire8Binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_questionnaire7, container, false
+            inflater, R.layout.fragment_questionnaire8, container, false
         )
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
@@ -34,12 +35,12 @@ class Questionnaire7Fragment : Fragment() {
 
         val navController = findNavController()
 
-        binding.questionnaire7BackButton.setOnClickListener {
+        binding.questionnaire8BackButton.setOnClickListener {
             navController.popBackStack()
         }
 
-        binding.questionnaire7NextButton.setOnClickListener {
-            navController.navigate(R.id.action_questionnaire7Fragment_to_questionnaire8Fragment)
+        binding.questionnaire8NextButton.setOnClickListener {
+            // TODO Questionnaire9Fragmentにnavigate
         }
     }
 
