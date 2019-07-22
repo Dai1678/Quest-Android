33f1c6e9661139dbf0a3c60f869919ea777fd6c2
Dai1678 <dai7774@gmail.com>
2019-07-22 19:16:33 +0900

    アンケート5問目のレイアウト、遷移処理完成

--- app/src/main/java/com/dai1678/quest/ui/questionnaire/Questionnaire5Fragment.kt
+++ app/src/main/java/com/dai1678/quest/ui/questionnaire/Questionnaire5Fragment.kt
@@ -1,27 +1,28 @@
 package com.dai1678.quest.ui.questionnaire
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+
 import com.dai1678.quest.R
-import com.dai1678.quest.databinding.CardViewQuestionnaire4Binding
-import com.dai1678.quest.databinding.FragmentQuestionnaire4Binding
+import com.dai1678.quest.databinding.CardViewQuestionnaire5Binding
+import com.dai1678.quest.databinding.FragmentQuestionnaire5Binding
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.databinding.BindableItem
 import com.xwray.groupie.databinding.ViewHolder
 
-class Questionnaire4Fragment : Fragment() {
+class Questionnaire5Fragment : Fragment() {
 
     private val viewModel: QuestionnaireViewModel by activityViewModels()
     private val groupAdapter = GroupAdapter<ViewHolder<*>>()
 
-    private lateinit var binding: FragmentQuestionnaire4Binding
+    private lateinit var binding: FragmentQuestionnaire5Binding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,7 +30,7 @@ class Questionnaire4Fragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_questionnaire4, container, false
+            inflater, R.layout.fragment_questionnaire5, container, false
         )
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
@@ -40,55 +41,55 @@ class Questionnaire4Fragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val questionnaireNumbers = resources.getStringArray(R.array.questionnaire_sub_numbers)
-        val questionnaireMessages = resources.getStringArray(R.array.questionnaire_4_sub_messages)
+        val questionnaireMessages = resources.getStringArray(R.array.questionnaire_5_sub_messages)
 
         val items = ArrayList<CardViewItem>()
         for (i in questionnaireMessages.indices)
             items.add(CardViewItem(questionnaireNumbers[i], questionnaireMessages[i]))
         groupAdapter.update(items)
 
-        binding.questionnaire4RecyclerView.apply {
+        binding.questionnaire5RecyclerView.apply {
             layoutManager = LinearLayoutManager(activity)
             adapter = groupAdapter
         }
 
         val navController = findNavController()
 
-        binding.questionnaire4BackButton.setOnClickListener {
+        binding.questionnaire5BackButton.setOnClickListener {
             navController.popBackStack()
         }
 
-        binding.questionnaire4NextButton.setOnClickListener {
-            navController.navigate(R.id.action_questionnaire4Fragment_to_questionnaire5Fragment)
+        binding.questionnaire5NextButton.setOnClickListener {
+            // TODO Questionnaire6Fragmentにnavigate
         }
     }
 
     inner class CardViewItem(
         private val questionnaireNumber: String,
         private val questionnaireMessage: String
-    ) : BindableItem<CardViewQuestionnaire4Binding>() {
+    ) : BindableItem<CardViewQuestionnaire5Binding>() {
 
-        override fun getLayout(): Int = R.layout.card_view_questionnaire_4
+        override fun getLayout(): Int = R.layout.card_view_questionnaire_5
 
-        override fun bind(viewBinding: CardViewQuestionnaire4Binding, position: Int) {
+        override fun bind(viewBinding: CardViewQuestionnaire5Binding, position: Int) {
             viewBinding.apply {
-                questionnaire4SubNumberText.text = questionnaireNumber
-                questionnaire4SubMessageText.text = questionnaireMessage
-                questionnaire4ExpandableLayout.isExpanded = position == 0
+                questionnaire5SubNumberText.text = questionnaireNumber
+                questionnaire5SubMessageText.text = questionnaireMessage
+                questionnaire5ExpandableLayout.isExpanded = position == 0
 
-                questionnaire4CardViewActionButton.apply {
-                    if (questionnaire4ExpandableLayout.isExpanded) {
+                questionnaire5CardViewActionButton.apply {
+                    if (questionnaire5ExpandableLayout.isExpanded) {
                         this.text = resources.getString(R.string.questionnaire_collapsed_text)
                     } else {
                         this.text = resources.getString(R.string.questionnaire_expand_text)
                     }
 
                     setOnClickListener {
-                        if (questionnaire4ExpandableLayout.isExpanded) {
-                            questionnaire4ExpandableLayout.isExpanded = false
+                        if (questionnaire5ExpandableLayout.isExpanded) {
+                            questionnaire5ExpandableLayout.isExpanded = false
                             this.text = resources.getString(R.string.questionnaire_expand_text)
                         } else {
-                            questionnaire4ExpandableLayout.isExpanded = true
+                            questionnaire5ExpandableLayout.isExpanded = true
                             this.text = resources.getString(R.string.questionnaire_collapsed_text)
                         }
                     }
