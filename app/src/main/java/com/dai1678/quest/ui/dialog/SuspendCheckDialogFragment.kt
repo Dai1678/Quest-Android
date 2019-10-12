efe7c6545d9fabde3ac00f5bd5d1bc172b5e22a4
Dai1678 <dai7774@gmail.com>
2019-10-12 23:06:26 +0900

    タブレット表示時のUI調整

--- app/src/main/java/com/dai1678/quest/ui/dialog/SuspendCheckDialogFragment.kt
+++ app/src/main/java/com/dai1678/quest/ui/dialog/SuspendCheckDialogFragment.kt
@@ -1,5 +1,6 @@
 package com.dai1678.quest.ui.dialog
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
@@ -8,10 +9,13 @@ import com.dai1678.quest.R
 
 class SuspendCheckDialogFragment : DialogFragment() {
 
+    @SuppressLint("InflateParams")
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
+        val view = requireActivity().layoutInflater.inflate(R.layout.dialog_suspend_check, null)
+
         return AlertDialog.Builder(requireContext(), R.style.AlertDialog_Style).apply {
-            setMessage(R.string.suspend_check_message)
+            setView(view)
             setPositiveButton(R.string.suspension_diagnosis) { _, _ ->
                 requireActivity().finish()
             }
