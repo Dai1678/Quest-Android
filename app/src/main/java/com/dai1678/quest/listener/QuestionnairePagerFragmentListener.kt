51d36afa46ee1663b1ce7f465f66c0c10b95d1c9
Dai1678 <dai7774@gmail.com>
2020-02-25 20:03:50 +0900

    QuestionnairePagerFragmentListenerへのコメント追加

--- app/src/main/java/com/dai1678/quest/listener/QuestionnairePagerFragmentListener.kt
+++ app/src/main/java/com/dai1678/quest/listener/QuestionnairePagerFragmentListener.kt
@@ -5,11 +5,13 @@ import android.view.View
 interface QuestionnairePagerFragmentListener {
     /**
      * 前に戻るボタンを押した時
+     * @param view View
      */
     fun onClickBack(view: View)
 
     /**
      * 次に進むボタンを押した時
+     * @param view View
      */
     fun onClickNext(view: View)
 }
