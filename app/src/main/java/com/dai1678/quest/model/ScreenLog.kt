f518fe29c415550dac072d994a6982c27aa6bfd0
Dai1678 <dai7774@gmail.com>
2020-02-25 23:59:33 +0900

    UserListItemへのコメント追加

--- app/src/main/java/com/dai1678/quest/model/ScreenLog.kt
+++ app/src/main/java/com/dai1678/quest/model/ScreenLog.kt
@@ -5,7 +5,7 @@ package com.dai1678.quest.model
  *
  * @param patientId 受検者のid
  * @param currentPage: 現在表示されている回答画面のページ番号
- * @param currentTime 現在時刻 //TODO Date型もしくは日付フォーマットされたString型にする
+ * @param currentTime 現在時刻 // TODO Date型もしくは日付フォーマットされたString型にする
  * @param scrollCount 回答画面内で設問表示を切り替えた回数
  * @param gender 受検者の性別
  * @param ageRange 受検者の年齢範囲
