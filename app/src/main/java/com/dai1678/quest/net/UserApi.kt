58a5d110e4c125ede8834a4e8979e7021556603a
Dai1678 <dai7774@gmail.com>
2020-02-25 22:14:22 +0900

    UserApiクラスへのコメント追加

--- app/src/main/java/com/dai1678/quest/net/UserApi.kt
+++ app/src/main/java/com/dai1678/quest/net/UserApi.kt
@@ -8,16 +8,32 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 
+/**
+ * ユーザー情報のAPIエンドポイントを定義したインターフェース
+ */
 interface UserApi {
-
+    /**
+     * ユーザー情報の取得
+     * @return PatientListResponse
+     */
     @GET("patients")
     suspend fun getUsers(): PatientListResponse
 
+    /**
+     * ユーザー情報の保存
+     * @param user ユーザー情報
+     * @return DefaultResponse
+     */
     @POST("patients")
     suspend fun createUser(
         @Body user: User
     ): DefaultResponse
 
+    /**
+     * ユーザー情報の取得
+     * @param userId ユーザーのid
+     * @return ユーザー情報
+     */
     @GET("patient/{id}")
     suspend fun getUser(
         @Path("id") userId: String
