744cc482fcca8141646ff403307299e375ea4dc2
Dai1678 <dai7774@gmail.com>
2020-02-25 20:05:54 +0900

    DefaultResponseへのコメント追加

--- app/src/main/java/com/dai1678/quest/model/DefaultResponse.kt
+++ app/src/main/java/com/dai1678/quest/model/DefaultResponse.kt
@@ -2,6 +2,9 @@ package com.dai1678.quest.model
 
 /**
  * APIから特定のデータを含まない場合に返されるデフォルトレスポンス
+ * @param statusCode ステータスコード
+ * @param isSuccess 処理が成功したかどうか
+ * @param message メッセージ
  */
 data class DefaultResponse(
     val statusCode: Int,
