e6e3da31d9e6384f50814e9f96c54f13cc606684
Dai1678 <dai7774@gmail.com>
2020-02-25 23:16:04 +0900

    QuestionnaireAnswerViewModelへのコメント追加

--- app/src/main/java/com/dai1678/quest/ui/questionnaire/QuestionnaireAnswerViewModel.kt
+++ app/src/main/java/com/dai1678/quest/ui/questionnaire/QuestionnaireAnswerViewModel.kt
@@ -17,7 +17,11 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 /**
- * 回答画面のViewModel
+ * 回答画面のViewModelオブジェクト
+ *
+ * @property questionMessage 設問文章
+ * @property answerChoiceMessages 回答項目文言
+ * @property questionnaireResult 回答結果
  */
 class QuestionnaireAnswerViewModel : ViewModel() {
     private val questionnaireRepository = QuestionnaireRepository.getInstance()
@@ -26,18 +30,20 @@ class QuestionnaireAnswerViewModel : ViewModel() {
     private val mutableSnackBarText = MutableLiveData<Event<Int>>()
     val snackBarText: LiveData<Event<Int>> = mutableSnackBarText
 
-    // 設問文章
     private val mutableQuestionMessage = MutableLiveData<String>()
     val questionMessage: LiveData<String> = mutableQuestionMessage
 
-    // 回答項目文言
     private val mutableAnswerChoiceMessages = MutableLiveData<Array<String>>()
     val answerChoiceMessages: LiveData<Array<String>> = mutableAnswerChoiceMessages
 
-    // 回答結果
     private var questionnaireResult = QuestionnaireResult()
 
-    // 回答結果の一時保存
+    /**
+     * 回答結果の一時保存
+     * @param page 現在表示している回答画面のページ番号
+     * @param answer 選択した回答項目の番号
+     * @param index 結果保存の配列のindex (小問の回答の場合のみ使用)
+     */
     fun setQuestionnaireResult(page: Int, answer: Int, index: Int = 0) {
         when (Question.valueOf(page)) {
             Question.PAGE1 -> questionnaireResult.page1Answer = answer
@@ -57,13 +63,20 @@ class QuestionnaireAnswerViewModel : ViewModel() {
         }
     }
 
-    // 設問文章と回答項目文言を更新
+    /**
+     * 設問文章と回答項目文言を更新
+     * @param message 更新後の設問文章
+     * @param answers 更新後の回答項目文言
+     */
     fun update(message: String, answers: Array<String>) {
         mutableQuestionMessage.value = message
         mutableAnswerChoiceMessages.value = answers
     }
 
-    // 結果送信ボタンを押したときの処理
+    /**
+     * 結果送信ボタンを押したときの処理
+     * @param userId ユーザーのid
+     */
     fun sendQuestionnaireResult(userId: String) {
         viewModelScope.launch {
             when (val result = postResult(userId)) {
@@ -79,7 +92,11 @@ class QuestionnaireAnswerViewModel : ViewModel() {
         }
     }
 
-    // 回答結果送信処理
+    /**
+     * 回答結果送信処理
+     * @param userId ユーザーのid
+     * @return NetworkResult<Questionnaire>
+     */
     private suspend fun postResult(userId: String): NetworkResult<Questionnaire> =
         withContext(Dispatchers.IO) {
             val questionnaire = Questionnaire(
@@ -89,13 +106,18 @@ class QuestionnaireAnswerViewModel : ViewModel() {
             questionnaireRepository.createResult(questionnaire)
         }
 
-    // 結果送信処理後のメッセージ設定
+    /**
+     * 結果送信処理後のメッセージ設定
+     * @param messageResId Toastに表示する文言の文字列リソース
+     */
     private fun showPostedResultMessage(messageResId: Int) {
         mutableSnackBarText.postValue(Event((messageResId)))
     }
 
     interface Callback {
-        // 結果送信完了後の処理
+        /**
+         * 結果送信完了後の処理
+         */
         fun finishQuestionnaire()
     }
 }
