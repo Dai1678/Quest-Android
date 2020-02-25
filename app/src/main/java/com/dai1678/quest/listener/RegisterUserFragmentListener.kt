a7ee0129b4c7ed6913e8b37e232174bb43c89ae7
Dai1678 <dai7774@gmail.com>
2020-02-25 20:04:30 +0900

    RegisterUserFragmentListenerへのコメント追加

--- app/src/main/java/com/dai1678/quest/listener/RegisterUserFragmentListener.kt
+++ app/src/main/java/com/dai1678/quest/listener/RegisterUserFragmentListener.kt
@@ -3,9 +3,9 @@ package com.dai1678.quest.listener
 import android.view.View
 
 interface RegisterUserFragmentListener {
-
     /**
-     * 登録ボタン
+     * 登録ボタンを押したとき
+     * @param view View
      */
     fun onClickRegisterUserData(view: View)
 }
