6c9ee26bb732ca554a1f4e507e42ba56bd336bef
Dai1678 <dai7774@gmail.com>
2020-02-25 19:34:54 +0900

    MainActivityListenerクラスにコメント追加

--- app/src/main/java/com/dai1678/quest/listener/MainActivityListener.kt
+++ app/src/main/java/com/dai1678/quest/listener/MainActivityListener.kt
@@ -3,6 +3,7 @@ package com.dai1678.quest.listener
 interface MainActivityListener {
     /**
      * Toolbarのtitleを設定
+     * @param title 変更後のToolbarのtitle
      */
     fun updateToolbarTitle(title: String)
 }
