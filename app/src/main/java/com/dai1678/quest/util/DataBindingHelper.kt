4e3ea413894d756066691742421c6ecb556f9af1
Dai1678 <dai7774@gmail.com>
2020-02-26 00:09:25 +0900

    DataBindingHelperへのコメント追加

--- app/src/main/java/com/dai1678/quest/util/DataBindingHelper.kt
+++ app/src/main/java/com/dai1678/quest/util/DataBindingHelper.kt
@@ -3,8 +3,16 @@ package com.dai1678.quest.util
 import android.view.View
 import androidx.databinding.BindingAdapter
 
+/**
+ * DataBindingの独自Adapterの定義クラス
+ */
 class DataBindingHelper {
     companion object {
+        /**
+         * trueの場合ViewをVISIBLE、falseの場合GONEに設定する
+         * @param view Visibleを操作する対象のView
+         * @param value Visibleのフラグ
+         */
         @JvmStatic
         @BindingAdapter("visibilityGone")
         fun setVisibilityGone(view: View?, value: Boolean?) {
@@ -15,6 +23,11 @@ class DataBindingHelper {
             }
         }
 
+        /**
+         * trueの場合ViewをVISIBLE、falseの場合INVISIBLEに設定する
+         * @param view Visibleを操作する対象のView
+         * @param value Visibleのフラグ
+         */
         @JvmStatic
         @BindingAdapter("visibilityInvisible")
         fun setVisibilityInvisible(view: View?, value: Boolean?) {
